88157f0dc9592c2e1fefacaea9ed1886f456ba57
jorge <jorgemartinez.jam@gmail.com>
2025-01-03 22:42:14 -0300

    feat: swagger, express validator, nexauth

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,35 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 import { loggerGlobal } from './middlewares/logger.middleware';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule);
+
   app.use(loggerGlobal);
-  await app.listen(3200)
+
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('API for my app')
+    .setVersion('1.0')
+    .addBearerAuth() 
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); 
+
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, 
+      forbidNonWhitelisted: true, 
+      transform: true, 
+    }),
+  );
+
+  await app.listen(3200);
+
+  console.log(`Application is running on: http://localhost:3200`);
 }
-bootstrap()
+bootstrap();
---
+++ frontend/src/app/home/page.tsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+    return (
+      <main className="flex items-center justify-center h-screen bg-gray-50">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Welcome to the Home Page!
+        </h1>
+      </main>
+    )
+  }
+  
