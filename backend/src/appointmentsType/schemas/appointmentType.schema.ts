194c3d590cfe62fd668ede4489ae311e52d33f48
fenixreds <114960950+fenixreds@users.noreply.github.com>
2025-01-07 16:15:20 -0500

    Appointments Mongodb test

---
+++ backend/src/appointmentsType/schemas/appointmentType.schema.ts
@@ -0,0 +1,53 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { Document, Schema as MongooseSchema } from 'mongoose'
+import { v4 as uuidv4 } from 'uuid'
+
+export type UserDocument = AppointmentType & Document
+
+@Schema({ timestamps: false })
+export class AppointmentType {
+  @Prop({
+    type: String,
+    default: uuidv4,
+  })
+  _id: string
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  name: string
+
+  @Prop({
+    type: String,
+    required: false,
+  })
+  remote_id?: string
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  display_name: string
+
+  @Prop({
+    type: Array,
+    default: [],
+  })
+  procedure_codes: Array<string>
+
+  @Prop({
+    type: String,
+    required: false,
+  })
+  appointment_length?: string
+
+  @Prop({
+    type: Object,
+    required: false,
+  })
+  additional_data?: Object
+
+}
+
+export const AppointmentTypeSchema = SchemaFactory.createForClass(AppointmentType)
