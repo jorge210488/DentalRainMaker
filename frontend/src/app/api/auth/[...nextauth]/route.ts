a52a863eb15ac92011ddcdce11f6810971c66868
jorge <jorgemartinez.jam@gmail.com>
2025-01-12 10:45:58 -0300

    fix: nextauth v1

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,55 @@
-// front/src/app/api/auth/[...nextauth]/route.ts
 import NextAuth, { AuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import CredentialsProvider from 'next-auth/providers/credentials'
 import { getFirebaseToken } from '@/utils/firebase'
 
-const handler = NextAuth({
+export const authOptions: AuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials) return null
+
+        try {
+          const response = await fetch(
+            `${process.env.NEXT_PUBLIC_API_URL}/auth/signin`,
+            {
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify({
+                email: credentials.email,
+                password: credentials.password,
+                provider: 'local',
+              }),
+            },
+          )
+
+          if (!response.ok) {
+            throw new Error('Invalid credentials')
+          }
+
+          const data = await response.json()
+
+          // Retornar los datos necesarios
+          return {
+            id: data.userId,
+            id_token: data.token,
+            user_id: data.userId,
+            user_type: data.type,
+          }
+        } catch (error) {
+          console.error('Error during local login:', error)
+          return null
+        }
+      },
     }),
   ],
   callbacks: {
@@ -24,7 +66,6 @@ const handler = NextAuth({
         }
 
         try {
-          // Attempt to register the user
           await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/signup`, {
             method: 'POST',
             headers: {
@@ -39,7 +80,6 @@ const handler = NextAuth({
         }
 
         try {
-          // Attempt to sign in the user
           const loginPayload = {
             email: profile.email,
             provider: 'google',
@@ -62,6 +102,10 @@ const handler = NextAuth({
           }
 
           const data = await response.json()
+
+          account.id_token = data.token
+          account.user_id = data.userId
+          account.user_type = data.type
 
           // Obtener el token de Firebase y enviarlo al backend
           const firebaseToken = await getFirebaseToken()
@@ -89,32 +133,39 @@ const handler = NextAuth({
       }
       return true // Allow sign-in
     },
-    async redirect({ url, baseUrl }) {
-      return '/home'
-    },
-    async jwt({ token, account, profile }) {
-      if (account && profile) {
-        token.id = account.providerAccountId // Google User ID
-        token.given_name = profile.given_name
-        token.family_name = profile.family_name
-        token.email = profile.email
-        token.name = profile.name
-        token.provider = 'google'
+
+    async jwt({ token, account, user }) {
+      if (account && account.provider === 'credentials') {
+        token.accessToken = user?.id_token // De los datos retornados
+        token.userId = user?.user_id
+        token.type = user?.user_type
       }
+
+      if (account && account.provider === 'google') {
+        token.accessToken = account.id_token
+        token.userId = account.user_id
+        token.type = account.user_type
+      }
+
       return token
     },
+
     async session({ session, token }) {
       if (session.user) {
         session.user.id = token.id as string
-        session.user.given_name = token.given_name as string
-        session.user.family_name = token.family_name as string
-        session.user.email = token.email as string
-        session.user.name = token.name as string
-        session.user.provider = token.provider as string
+        session.user.token = token.accessToken as string
+        session.user.userId = token.userId as string
+        session.user.type = token.type as string
       }
+
       return session
     },
+    async redirect({ url, baseUrl }) {
+      return '/home'
+    },
   },
-})
+}
 
+// Exporta el handler de NextAuth
+const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST }
