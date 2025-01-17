0a73c9efd479f6b85a4db9ee3dba0f31282567bc
jorge <jorgemartinez.jam@gmail.com>
2025-01-17 00:57:20 -0300

    fix: routes

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,17 +1,24 @@
 'use client'
 
+import { usePathname } from 'next/navigation'
 import { SessionProvider } from 'next-auth/react'
 import { Provider } from 'react-redux'
 import store from '@/redux/store'
 import './globals.css'
 import FirebaseNotification from '@/components/firebaseNotification'
 import AppInitializer from '@/components/appInitializer'
+import Header from '@/components/patientDashboard/header' // Importa tu componente Header
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const pathname = usePathname()
+
+  // Excluir el Header en páginas específicas
+  const excludeHeader = ['/login', '/register']
+
   return (
     <html lang='en'>
       <body>
@@ -19,7 +26,11 @@ export default function RootLayout({
           <Provider store={store}>
             {/* Componente que maneja el guardado del token */}
             <FirebaseNotification />
-            <AppInitializer>{children}</AppInitializer>
+            <AppInitializer>
+              {/* Mostrar el Header solo si la ruta no está en la lista de exclusión */}
+              {!excludeHeader.includes(pathname) && <Header />}
+              {children}
+            </AppInitializer>
           </Provider>
         </SessionProvider>
       </body>
