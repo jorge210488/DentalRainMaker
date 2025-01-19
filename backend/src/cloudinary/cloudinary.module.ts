4612db302826964c0a4809fa22374858eb443af4
jorge <jorgemartinez.jam@gmail.com>
2025-01-19 12:58:25 -0300

    fix: cloudinary with kolla

--- backend/src/cloudinary/cloudinary.module.ts
+++ backend/src/cloudinary/cloudinary.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common'
 import { CloudinaryService } from './cloudinary.service'
 import { CloudinaryController } from './cloudinary.controller'
-import { MongooseModule } from '@nestjs/mongoose'
-import { User, UserSchema } from '../users/schemas/user.schema'
 import { CloudinaryConfig } from '../config/cloudinary'
 import { AuthModule } from 'src/auth/auth.module'
+import { ContactsModule } from 'src/contacts/contacts.module'
+import { MongooseModule } from '@nestjs/mongoose'
+import { Cloudinary, CloudinarySchema } from './schemas/cloudinary.schema'
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: Cloudinary.name, schema: CloudinarySchema },
+    ]),
+    ContactsModule,
     AuthModule,
   ],
   providers: [CloudinaryService, CloudinaryConfig],
