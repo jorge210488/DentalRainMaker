66caef6644f329db46d7b2671fac135e78d90054
jorge <jorgemartinez.jam@gmail.com>
2025-01-21 17:25:55 -0300

    fix: to change branches

--- frontend/src/app/adminDashboard/layout.tsx
+++ frontend/src/app/adminDashboard/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Sidebar from '@/components/patientDashboard/sidebar'
+// import Sidebar from '@/components/patientDashboard/sidebar'
 import Header from '@/components/patientDashboard/header'
 import { DashboardShell } from '@/components/AdminDashboard/dashboard-shell'
 import { DashboardHeader } from '@/components/patientDashboard/dashboard-header'
@@ -24,10 +24,10 @@ export default function RootLayout({
           <DashboardShell />
           <div className='flex flex-1 flex-col overflow-hidden'>
             <DashboardHeader
-                    // heading={`Welcome back, ${session?.user?.name || 'Guest'}`}
-                    heading='Welcome back, Doctor'
-                    text='Manage your dental care journey'
-                  />
+              // heading={`Welcome back, ${session?.user?.name || 'Guest'}`}
+              heading='Welcome back, Doctor'
+              text='Manage your dental care journey'
+            />
             <main className='flex-1 overflow-y-auto p-4'>{children}</main>
           </div>
         </div>
