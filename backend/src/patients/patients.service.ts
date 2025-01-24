d33a3bf97f16736a39a2653bb0becbc5ae01f78b
fenixreds <114960950+fenixreds@users.noreply.github.com>
2025-01-24 15:49:55 -0500

    test get patients fast ok

--- backend/src/patients/patients.service.ts
+++ backend/src/patients/patients.service.ts
@@ -44,33 +44,27 @@ export class PatientService {
       // Obtener datos de la API externa
       const { url, headers } = await this.getRequestConfig(clinicId)
 
-      const response = await lastValueFrom(this.httpService.get(url, { headers }));
-      const contacts = response.data.contacts;  
-
-      // Filtrar los pacientes y calcular la edad
-      // const patients = contacts
-      //   .filter(contact => contact.type === 'PATIENT') // Filtrar por tipo "patient"
-      //   .map((patient) => ({
-      //     ...patient,
-      //     fullname: `${patient.given_name} ${patient.family_name}`,
-      //     age: this.calculateAge(patient.birth_date), // Calcular la edad
-      //     nextVisit: null,
-      //     lastVisit: null,
-      //     insurance: null,
-      //     activeTreatment: false,
-      //   }));
-
-      const listVisitsPatients = await this.appointmentsService.getVisits(clinicId);  
-      //   const listInsurancePatients = await this.insuranceService.getAllInsuranceCoverage(clinicId);
+      const [response, listInsurancePatients, listVisitsPatients] = await Promise.all([
+        lastValueFrom(this.httpService.get(url, { headers })),
+        this.insuranceService.getAllInsuranceCoverage(clinicId),
+        this.appointmentsService.getVisits(clinicId)
+      ]) 
+      
+      const contacts = response.data.contacts;
 
       const visitsMap: Record<string, { contactId: string; nextVisit: string | null; lastVisit: string | null }> = Object.fromEntries(
         listVisitsPatients.map((visit) => [visit.contactId, visit])
+      );
+
+      const insurancesMap: Record<string, {contactId: string; carrier_title: string | null}> = Object.fromEntries(
+        listInsurancePatients.map((insurance) => [insurance.contactId, insurance])
       );
       
       const patients = contacts
         .filter((contact) => contact.type === 'PATIENT')
         .map((patient) => {
           const visitData = visitsMap[patient.remote_id];
+          const insuranceData = insurancesMap[patient.remote_id];
       
           return {
             ...patient,
@@ -78,29 +72,13 @@ export class PatientService {
             age: this.calculateAge(patient.birth_date),
             nextVisit: visitData?.nextVisit || null,
             lastVisit: visitData?.lastVisit || null,
-            insurance: null,
+            insurance: insuranceData?.carrier_title || null,
             activeTreatment: false,
           };
         });
       
       return patients;
       
-      // // Obtener seguro y visitas para cada paciente
-      // const patientsWithVisits = await Promise.all(
-      //   patients.map(async (patient) => {
-      //     const { nextVisit, lastVisit } = await this.appointmentsService.getVisits(clinicId, patient.remote_id);
-      //     const  insuranceArray = await this.insuranceService.getInsurance(clinicId, patient.remote_id);
-          
-      //     return {
-      //       ...patient,
-      //       insurance: insuranceArray?.length > 0 ? insuranceArray[0]?.subscriber || null : null,
-      //       nextVisit,
-      //       lastVisit,
-      //     };
-      //     }),
-      //   );  
-
-      // return patientsWithVisits;
     } catch (error) {
       throw new Error('Error fetching patients: ' + error.message);
     }
